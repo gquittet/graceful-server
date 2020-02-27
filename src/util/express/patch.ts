284a088accee2f20184024b1ead48e7b198aabba
Guillaume Quittet <guillaume.quittet@gmail.com>
2020-02-27 15:45:12 +0100

    fix: set /live and /ready as first middleware
    To fix the error of Error [ERR_HTTP_HEADERS_SENT]:
    Cannot set headers after they are sent to the client
    I've move liveness and readiness from the end of the middlwares stack
    to the front. So with this
    solution I can bypass the express way of handling routes.
    Now we can enable GracefulServer from anywhere.
    Before this fix, we need to setup GracefulServer before the express
    endpoints and middleware.

--- src/util/express/patch.ts
+++ src/util/express/patch.ts
@@ -5,9 +5,15 @@ const { livenessEndpoint, readinessEndpoint } = config
 
 const patch = {
   apply: (app: Express) => {
-    app.disable('x-powered-by')
+    // Add empty endpoints for liveness and readiness in express.
+    // With this technique, express will recognize the liveness and readiness endpoint.
+    // But it will do nothing.
     app.get(livenessEndpoint, () => {})
     app.get(readinessEndpoint, () => {})
+    // Push these endpoints at the beginning of the middleware stack to avoid
+    // Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client.
+    app._router.stack.unshift(app._router.stack.pop())
+    app._router.stack.unshift(app._router.stack.pop())
   }
 }
 
