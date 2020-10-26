42c262eec5c83faed638e35e8db6135c1c37905f
Guillaume Quittet <guillaume.quittet@gmail.com>
2020-10-26 10:59:50 +0100

    refactor: improve express patch code

--- src/util/express/patch.ts
+++ src/util/express/patch.ts
@@ -9,10 +9,10 @@ const patch = {
     // But express will do nothing.
     // Thus, we avoid this kind of error:
     // Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client.
-    const oldExpressHandler = app.handle
-    app.handle = function (req: any, res: any, next: any) {
+    const expressAppHandler = app.handle
+    app.handle = (req: any, res: any, next: any) => {
       if (req.url !== livenessEndpoint && req.url !== readinessEndpoint) {
-        oldExpressHandler.call(app, req, res, next)
+        return expressAppHandler.call(app, req, res, next)
       }
     }
   }
