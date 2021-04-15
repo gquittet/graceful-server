f2af0a38775d595fcdc31ba61e8f01dda189c9dd
Guillaume Quittet <guillaume.quittet@gmail.com>
2021-04-15 11:13:39 +0200

    feat: wait gracePeriod if setup
    ISSUE-5

--- src/util/shutdown.ts
+++ src/util/shutdown.ts
@@ -1,13 +1,18 @@
 import config from '~/config'
+import { MILLISECONDS_IN_ONE_SECOND } from '~/constants'
 import State from '~/core/state'
 import ICore from '~/interface/core'
 import ImprovedServer from '~/interface/improvedServer'
 import sleep from './sleep'
 
 const shutdown = (server: ImprovedServer, parent: ICore) => async (type: string, value: number, body?: Error) => {
-  const { timeout, closePromises } = config
+  const { timeout, closePromises, gracePeriod } = config
 
   const error: Error = body && body.message ? body : new Error(type)
+
+  if (gracePeriod > 0) {
+    await sleep(gracePeriod * MILLISECONDS_IN_ONE_SECOND)
+  }
 
   parent.status.set(State.SHUTTING_DOWN, error)
   await sleep(timeout)
