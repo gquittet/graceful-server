f4f3519234ab16fbabd46a45f4920c82e0f61451
Guillaume Quittet <guillaume.quittet@gmail.com>
2021-05-04 23:54:28 +0200

    feat: destroy socket depending on kubernetes mode

--- src/core/improvedServer.ts
+++ src/core/improvedServer.ts
@@ -9,7 +9,7 @@ import SocketsPool from './socketsPool'
 const { livenessEndpoint, readinessEndpoint } = config
 
 const improvedServer = (server: Server, serverStatus: IStatus): ImprovedServer => {
-  const { healthCheck } = config
+  const { healthCheck, kubernetes } = config
   const socketsPool = SocketsPool()
   const secureSocketsPool = SocketsPool()
   let stopping = false
@@ -28,21 +28,27 @@ const improvedServer = (server: Server, serverStatus: IStatus): ImprovedServer =
     server.on('request', onRequest(serverStatus))
 
     // Add the user listeners
-    listeners.forEach((listener: any) =>
+    listeners.forEach(listener =>
       server.on('request', (req: http.IncomingMessage, res: http.ServerResponse) => {
         // If the server is ready, call the listener. Else destroy the socket
-        if (serverStatus.isReady()) {
-          if ((req.url !== livenessEndpoint && req.url !== readinessEndpoint) || req.method !== 'GET') {
-            return listener(req, res)
-          }
-        } else {
-          req.socket.destroy()
+        // Kubernetes handles the traffic so when the server is shutting down we have
+        // to keep socket and active traffic.
+        const hasToDestroySocket = kubernetes
+          ? !serverStatus.isReady() && !serverStatus.isShuttingDown()
+          : !serverStatus.isReady()
+
+        if (hasToDestroySocket) return req.socket.destroy()
+
+        // Check if the call is not for liveness or readiness.
+        // If the call is not for liveness or readiness we can handle it.
+        if ((req.url !== livenessEndpoint && req.url !== readinessEndpoint) || req.method !== 'GET') {
+          return listener(req, res)
         }
       })
     )
   } else {
     // Add the user listeners only
-    listeners.forEach((listener: any) =>
+    listeners.forEach(listener =>
       server.on('request', (req: http.IncomingMessage, res: http.ServerResponse) => {
         // If the server is ready, call the listener. Else destroy the socket
         if (serverStatus.isReady()) {
