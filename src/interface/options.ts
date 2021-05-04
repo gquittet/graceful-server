861ee09de48953c6a9cb0909a695a5fd2d0f420a
Guillaume Quittet <guillaume.quittet@gmail.com>
2021-05-04 23:53:04 +0200

    feat: add kubernetes mode

--- src/interface/options.ts
+++ src/interface/options.ts
@@ -2,6 +2,7 @@ export default interface IOptions {
   closePromises: (() => Promise<unknown>)[]
   timeout: number
   healthCheck: boolean
+  kubernetes: boolean
   livenessEndpoint: string
   readinessEndpoint: string
   gracePeriod: number
