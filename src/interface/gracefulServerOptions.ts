88da38b45bf149f616d78fe2222497120ae84491
cibergarri <cibergarri@hotmail.com>
2021-05-15 01:24:20 +0200

    chore: add kubernetes to IGracefulServerOptions interface

--- src/interface/gracefulServerOptions.ts
+++ src/interface/gracefulServerOptions.ts
@@ -2,6 +2,7 @@ export default interface IGracefulServerOptions {
   closePromises?: (() => Promise<unknown>)[]
   timeout?: number
   healthCheck?: boolean
+  kubernetes?: boolean
   livenessEndpoint?: string
   readinessEndpoint?: string
 }
